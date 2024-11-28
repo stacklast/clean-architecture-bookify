8db449abe278c07e4012cc1d5869d455989c5606
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2024-11-28 15:37:57 -0500

    refactor: Using Double Dispatch for a better domain model

--- Bookify.Domain/Bookings/Booking.cs
+++ Bookify.Domain/Bookings/Booking.cs
@@ -1,4 +1,5 @@
 ﻿using Bookify.Domain.Abstractions;
+using Bookify.Domain.Apartments;
 using Bookify.Domain.Bookings.Events;
 using Bookify.Domain.Shared;
 
@@ -48,16 +49,17 @@ public sealed class Booking : Entity
     public DateTime? CancelledOnUtc { get; private set; }
 
     public static Booking Reserve(
-        Guid apartmentId,
+        Apartment apartment,
         Guid userId,
         DateRange duration,
         DateTime utcNow,
-        PricingDetails pricingDetails)
+        PricingService pricingService)
     {
+        var pricingDetails = pricingService.CalculatePrice(apartment, duration);
         var booking = new Booking(
             Guid.NewGuid(),
             userId,
-            apartmentId,
+            apartment.Id,
             duration,
             pricingDetails.PriceForPeriod,
             pricingDetails.CleaningFee,
@@ -66,6 +68,9 @@ public sealed class Booking : Entity
             BookingStatus.Reserved,
             utcNow);
         booking.RaiseDomainEvent(new BookingReservedDomainEvent(booking.Id));
+
+        apartment.LastBookedOnUtc = utcNow;
+
         return booking;
     }
 
