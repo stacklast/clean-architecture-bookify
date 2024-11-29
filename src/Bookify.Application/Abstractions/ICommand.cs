ebb3b6fb55fabec2c9268eedb25533fb077d7d3e
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2024-11-29 10:45:03 -0500

    feat: Add abstractions for CQRS

---
+++ src/Bookify.Application/Abstractions/ICommand.cs
@@ -0,0 +1,13 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Abstractions;
+
+public interface ICommand : IRequest<Result>
+{
+
+}
+public interface ICommand<TResponse> : IRequest<Result<TResponse>>
+{
+
+}
---
+++ src/Bookify.Application/Messaging/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Messaging;
+
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+{
+
+}
