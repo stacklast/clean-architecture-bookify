22ddb9f34c5414e9e5d49c3627474fa69cfeac23
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2024-11-30 16:44:40 -0500

    feat: Publish domain events inside the Unit of Work

--- src/Bookify.Infrastructure/ApplicationDBContext.cs
+++ src/Bookify.Infrastructure/ApplicationDBContext.cs
@@ -1,19 +1,53 @@
 ﻿using Bookify.Domain.Abstractions;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookify.Infrastructure;
 
-public sealed class ApplicationDBContext : DbContext, IUnitOfWork
+
+public sealed class ApplicationDbContext : DbContext, IUnitOfWork
 {
-    public ApplicationDBContext(DbContextOptions options)
+    private readonly IPublisher _publisher;
+
+    public ApplicationDbContext(DbContextOptions options, IPublisher publisher)
         : base(options)
     {
+        _publisher = publisher;
     }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDBContext).Assembly);
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
 
         base.OnModelCreating(modelBuilder);
+    }
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+
+        await PublishDomainEventsAsync();
+        return result;
+    }
+
+    private async Task PublishDomainEventsAsync()
+    {
+        var domainEvents = ChangeTracker
+            .Entries<Entity>()
+            .Select(entry => entry.Entity)
+            .SelectMany(entity =>
+            {
+                var domainEvents = entity.GetDomainEvents();
+
+                entity.ClearDomainEvents();
+
+                return domainEvents;
+            })
+            .ToList();
+
+        foreach (var domainEvent in domainEvents)
+        {
+            await _publisher.Publish(domainEvent);
+        }
     }
 }
