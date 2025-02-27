4ee407ec1e3691ebb9380f0d993d52dd6e226ad9
Edwin Benalcazar <softwareywebsoluciones@gmail.com>
2025-02-27 13:00:20 -0500

    feat: Integration Test - Add BaseIntegrationTest

---
+++ test/Bookify.Application.IntegrationTests/Infrastructure/BaseIntegrationTest.cs
@@ -0,0 +1,23 @@
+﻿using Bookify.Infrastructure;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Bookify.Application.IntegrationTests.Infrastructure;
+
+/// <summary>
+/// allow inject factory to the constructors
+/// </summary>
+public abstract class BaseIntegrationTest : IClassFixture<IntegrationTestWebAppFactory>
+{
+
+    private readonly IServiceScope _scope;
+    protected readonly ISender Sender;
+    protected readonly ApplicationDbContext DbContext;
+
+    protected BaseIntegrationTest(IntegrationTestWebAppFactory factory)
+    {
+        _scope = factory.Services.CreateScope();
+        Sender = _scope.ServiceProvider.GetRequiredService<ISender>();
+        DbContext = _scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    }
+}
